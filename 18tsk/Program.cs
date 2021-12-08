138c694285b9566b1b64a743cb7496df0f8a4aef
Вадим <Vadimk89@mail.ru>
2021-12-08 22:16:20 +0500

    Добавьте файлы проекта.

---
+++ 18tsk/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18tsk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку содержащую закрывающиеся скобки (круглые, квадратные и фигурные)");
+            string str = Console.ReadLine();
+            Console.WriteLine(ChkBracket(str) ? "Все скобки закрыты" : "Есть незакрытые скобки");
+
+            Console.ReadKey();
+        }
+        static bool ChkBracket(string str)
+        {
+            string br = "{}[]()";
+            Stack<char> cb = new Stack<char>();
+            char[] arr;
+            arr = str.ToCharArray();
+            foreach (char x in arr)
+            {                
+                if (x==skob.ContainsValue('('))
+                {
+
+                }
+            }
+           
+            if (cb.Count != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ 18tskk/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18tskk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку содержащую закрывающиеся скобки (круглые, квадратные и фигурные)");
+            string str = Console.ReadLine();
+            Console.WriteLine(ChkBracket(str)?"Все скобки закрыты":"Есть незакрытые скобки");
+            Console.ReadKey();
+        }
+        static bool ChkBracket(string str)
+        {
+            var skob = new Dictionary<char, char>
+            {
+                {')','('},
+                {'}','{'},
+                {']','['},
+            };
+            Stack<char> cb = new Stack<char>();
+            
+            foreach (var x in str)
+            {
+
+                if (x == '('||x=='{'||x=='[')
+                {
+                    cb.Push(x);
+                }
+                if (x == ')' || x == '}' || x == ']')
+                {
+                    if (cb.Count==0||cb.Pop()!=skob[x])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (cb.Count!=0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+
+}
